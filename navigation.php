aca2c731cf081378d301dc1f515ce0dbb424615c
inzeshtahir <inzesh14@gmail.com>
2025-01-15 18:14:49 -0500

    Create navigation.php

---
+++ navigation.php
@@ -0,0 +1,6 @@
+<div class="nav">
+    <a href="index.php">All Invoices</a>
+    <a href="draft.php">Draft Invoices</a>
+    <a href="pending.php">Pending Invoices</a>
+    <a href="paid.php">Paid Invoices</a>
+</div>
